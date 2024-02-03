c2b28f7cd974765966e22331532a9dc2eaff6b18
Mirabbos995 <jahonaliyevmirabbos621@gmail.com>
2024-02-03 15:10:38 +0300

    py

--- main.py
+++ main.py
@@ -1,57 +1,99 @@
-         # Sum of Resistance in Series Circuits
-
-a = [1, 5, 6.8, 3.2, -8]
-def circuit(a):
-    result = 1
-    for b in a:
-        result = result + b
-    print(result)
-circuit(a)
-
-
-
-
-         # Let's Sort This List!
-list = [5, 8, 1, 9, 2]
-def lst(list):
-    list.sort()
-    print(list)
-lst(list)
-
-
-get = [7, 8, 30, 78]
-def list(get):
-    get.reverse()
-    print(get)
-list(get)
-
-
-
-
-
-          # Number Split
-split = [2, 8, 10]
-num = 1/2
-def splt():
-    for n in split:
-        b = n * num
-        print(b)
-splt()
-
-
-
-
-
-          # Instant JAZZ
-jazz = ["G", "F", "C"]
-lst = []
-def chord():
-    for a in jazz:
-        d = a + "7"
-        lst.append(d)
-        print(lst)
-chord()
-
-
+#          # Sum of Resistance in Series Circuits
+#
+# a = [1, 5, 6.8, 3.2, -8]
+# def circuit(a):
+#     result = 1
+#     for b in a:
+#         result = result + b
+#     print(result)
+# circuit(a)
+#
+#
+#
+#
+#          # Let's Sort This List!
+# list = [5, 8, 1, 9, 2]
+# def lst(list):
+#     list.sort()
+#     print(list)
+# lst(list)
+#
+#
+# get = [7, 8, 30, 78]
+# def list(get):
+#     get.reverse()
+#     print(get)
+# list(get)
+#
+#
+#
+#
+#
+#           # Number Split
+# split = [2, 8, 10]
+# num = 1/2
+# def splt():
+#     for n in split:
+#         b = n * num
+#         print(b)
+# splt()
+#
+#
+#
+#
+#
+#           # Instant JAZZ
+# jazz = ["G", "F", "C"]
+# lst = []
+# def chord():
+#     for a in jazz:
+#         d = a + "7"
+#         lst.append(d)
+#         print(lst)
+# chord()
 
 
+#           # Unpacking lists
+# lst = [1, 2, 3, 4, 5, 6]
+# first = lst[0]
+# middle = lst[1:-1]
+# last = lst[-1]
+#
+# print(first)
+# print(middle)
+# print(last)
+
+
+
+
+          # Find the Odd Integer
+# not done
+
+          # Filter Strings from Array
+# not done
+
+          # Characters in Shapes
+# not done
+
+          # Moving to the end
+# def move(end):
+#     for a in end:
+#         d = end.pop(0)
+#         h = 5
+#         end.insert(d,h)
+#         print(end)
+# end = [1, 3, 2, 4, 4, 1]
+# move(end)
+
+# not done
+
+
+#           # Even Number Generator
+# def even_num(b):
+#     for d in range(b + 1):
+#         if d % 2 == 0:
+#             print(d)
+#     return
+# num = 10
+# a = even_num(num)
+# print(a)
